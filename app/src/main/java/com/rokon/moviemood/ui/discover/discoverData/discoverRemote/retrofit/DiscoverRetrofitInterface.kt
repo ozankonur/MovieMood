a9952aa67a1cba9b786884e5245a3c765fabdcb9
ozankonur <ozankonur@gmail.com>
2021-02-03 16:12:18 +0300

    API KEY Moved
    API KEY Moved to gradle

--- app/src/main/java/com/rokon/moviemood/ui/discover/discoverData/discoverRemote/retrofit/DiscoverRetrofitInterface.kt
+++ app/src/main/java/com/rokon/moviemood/ui/discover/discoverData/discoverRemote/retrofit/DiscoverRetrofitInterface.kt
@@ -1,9 +1,10 @@
 package com.rokon.moviemood.ui.discover.discoverData.discoverRemote.retrofit
 
+import com.rokon.moviemood.BuildConfig
 import com.rokon.moviemood.ui.discover.discoverData.discoverRemote.entity.DiscoverApiResponseEntity
 import retrofit2.http.GET
 
 interface DiscoverRetrofitInterface {
-    @GET("discover/movie?api_key=8abb11abf63bf9f48700f3e70d6bb7f9")
+    @GET("discover/movie?api_key=" + BuildConfig.API_KEY)
     suspend fun getMoviesForDiscover(): DiscoverApiResponseEntity
 }
